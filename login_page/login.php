adcd4928281efc57ee05eb5583cac185b9b241d7
ereinhardt6916 <ereinhardt6916@conestogac.on.ca>
2020-06-09 10:33:53 -0400

    fixed login

--- login_page/login.php
+++ login_page/login.php
@@ -6,9 +6,7 @@ $password = $_POST['password'];
 if($username == "username"&&$password=="password"){
     $_SESSION['username']=$username;
     require '../index_page/index.php';
-    require'../login_page/sucess_LI.html';
-    echo "<p>Congratulations, you are now logged in</p>";
-    echo '<a href="member.php">Click here</a>';
+   // require'../login_page/sucess_LI.html';
 }
 else{
     require '../login_page/login.html';
