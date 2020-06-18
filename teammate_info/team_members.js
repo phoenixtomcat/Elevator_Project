c1827120a3373a721a633b329bfb03ffb495d20b
WeipengTang <samwisetang@hotmail.com>
2020-06-18 13:51:08 -0400

    Add several small fixings for software assignment

--- teammate_info/team_members.js
+++ teammate_info/team_members.js
@@ -2,8 +2,8 @@ var today = new Date(); // new Date() object with current date and time
 var year = today.getFullYear();
 var e_birth = new Date('August 29, 1998 15:30:00'); // new Date() object with a value
 var n_birth = new Date('October 27, 1998 18:02:00'); // new Date() object with a value
-var r_birth = new Date('August 29, 1998 15:30:00'); // new Date() object with a value
-var w_birth = new Date('August 29, 1998 15:30:00'); // new Date() object with a value
+var r_birth = new Date('April 3, 1995 15:30:00'); // new Date() object with a value
+var w_birth = new Date('October 18, 1989 15:30:00'); // new Date() object with a value
 
 
 var age_e = today.getTime() - e_birth.getTime(); // Age in milliseconds
