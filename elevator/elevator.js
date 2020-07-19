05c8c7b134b2dbd5b39965ccdce9f6f3268f0330
ereinhardt6916 <ereinhardt6916@conestogac.on.ca>
2020-07-18 22:44:34 -0400

    got elevator gui working to reflect the database on my machine.  Have not tested with the real one.

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -79,7 +79,7 @@ function initElevator(){
 }
 
 
-function myMove(floor, pot) {
+function myMove(floor) {
     var target_raw_floor_number;
     if (floor == "1") {
         target_raw_floor_number = 465;
@@ -100,7 +100,6 @@ function myMove(floor, pot) {
             floor_audio(floor);
             open_door();
            // setTimeout(close_door(), 1000);
-            cha_back(pot);
 
         } else if (pos < target_raw_floor_number) {
             pos++;
@@ -196,40 +195,40 @@ function cha_col(pot) {
         case "dwnfl2":
             document.getElementById('dwnfl2').src = '../images/Buttons/Buttons for Eduard/Buttons--10.png';
             setLogic(pot);           
-            setTimeout(cha_col2, 1000);
+            //setTimeout(cha_col2, 1000);
             break;
 
         case "upfl2":
             document.getElementById('upfl2').src = '../images/Buttons/Buttons for Eduard/Buttons--09.png';
             setLogic(pot);
-            setTimeout(cha_col3, 1000);
+            //setTimeout(cha_col3, 1000);
             break;
 
         case "upfl1":
             document.getElementById('upfl1').src = '../images/Buttons/Buttons for Eduard/Buttons--09.png';
             setLogic(pot);
-            setTimeout(cha_col4, 1000);
+            //setTimeout(cha_col4, 1000);
             break;
 
         case "elv1":
             document.getElementById('elv1').src = '../images/Buttons/Buttons for Eduard/Buttons--02.png';
             //setFloorDB(1);
             setLogic(pot);
-            myMove(1, pot);
+            //myMove(1, pot);
             break;
 
         case "elv2":
             document.getElementById('elv2').src = '../images/Buttons/Buttons for Eduard/Buttons--04.png';
             //setFloorDB(2);      
             setLogic(pot);
-            myMove(2, pot);
+            //myMove(2, pot);
             break;
 
         case "elv3":
             document.getElementById('elv3').src = '../images/Buttons/Buttons for Eduard/Buttons--06.png';
             //setFloorDB(3);
             setLogic(pot);
-            myMove(3, pot);
+            //myMove(3, pot);
             break;
     }
 }
@@ -266,6 +265,57 @@ function cha_back(pot) {
     }
 }
 
+function GUIgetFloorDB(callback){
+    var xmlhttpShow = new XMLHttpRequest();
+    xmlhttpShow.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            if (typeof callback === 'function')
+                callback(this.responseText);
+        }
+    }
+    xmlhttpShow.open('GET', 'updateGui.php?q=null', true);
+    xmlhttpShow.send();
+
+}
+
+//Callback function to init elevator position
+function elevatorRunCallback(result){
+    floor_db = result;
+    moveElevator();
+}
+
+function moveElevator(){
+    //Since AJAX is async, this function has to be called inside the callback function
+
+    //update GUI floor number
+    floor_sign(parseInt(floor_db));
+
+    //Move GUI elevator to corrent floor
+    var elem = document.getElementById("elevator1");
+    switch (floor_db){
+        case "3":
+            myMove(3)
+            cha_back("dwnfl3");
+            cha_back("elv3");
+
+            break;
+        case "2":
+            myMove(2)
+            cha_back("dwnfl2");
+            cha_back("upfl2");
+            cha_back("elv2");
+            break;
+        case "1":
+            myMove(1)
+            cha_back("upfl1");
+            cha_back("elv1");
+            break;
+    }
+}
+
+var statusIntervalId = window.setInterval(function(){GUIgetFloorDB(elevatorRunCallback)}, 1000);
+
+//document.addEventListener("DOMContentLoaded", function(){updatGui()}, false);
 
 //Update elevator info when page loads
 document.addEventListener("DOMContentLoaded", function(){getFloorDB(elevatorInitCallback)}, false);
