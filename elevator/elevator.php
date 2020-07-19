66af76646598475e7345a55e29f1ad1e54b7cfee
WeipengTang <samwisetang@hotmail.com>
2020-07-19 16:04:17 -0400

    adapt to only authorized people can access elevator page

--- elevator/elevator.php
+++ elevator/elevator.php
@@ -4,8 +4,16 @@ session_start();
 
 if (isset($_SESSION['username'])){
 
-    require '../top_header/bar_LI.html';
-    require 'elevator.html';
+    if(isset($_SESSION['authorized']) && $_SESSION['authorized'] == 1){
+        //only authorized person can control the elevator
+        require '../top_header/bar_LI.html';
+        require 'elevator.html';
+    }
+    else{
+        require "../login_page/not_authorized.html";
+        require '../index_page/index.php';
+    }
+    
 
 } else{
     require '../login_page/login.php';
