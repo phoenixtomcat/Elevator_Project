ecc87ee4a75e23cec3234e7fb6d36a3704fac1e4
ereinhardt6916 <ereinhardt6916@conestogac.on.ca>
2020-07-27 13:56:03 -0400

    log book and small GUI changes

--- elevator/updateGui.php
+++ elevator/updateGui.php
@@ -39,7 +39,52 @@ function getFloorDB(){
     }
 }
 
-echo getFloorDB();
+function getStatusDB(){
+    /* to get current floor number from database
+    return: current floor -- if SQL execution succeeded and got non-empty array
+            null          -- execution failed or got empty array */
+    //database parameters
+    $db = new PDO(
+        'mysql:host=127.0.0.1;dbname=project_database',
+        'ese',
+        'ese'
+    );
+
+    //return arrays with keys that are the name of the fields
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    //prepare SQL statement
+    $query = 'SELECT status FROM elevatorControl WHERE nodeID = 0;';
+    $statement = $db->prepare($query);
+
+    //execute the SQL statement and store return array at $row
+    $result = $statement->execute();
+    $row = $statement->fetchAll();
+
+    //check execution result
+    if (!$result){
+        //execution failed
+        return null;
+    }else{
+        //execution successfully
+        if ($row == [])
+            //get empty result back
+            return null;
+        else
+            //get current floor
+            return $row[0]['status'];
+    }
+}
+
+
+if ($floor_cmd == "null"){
+    //request current floor from database
+    echo getFloorDB();
+}else {
+        echo setStatusDB();
+    
+
+}
 
 
 
